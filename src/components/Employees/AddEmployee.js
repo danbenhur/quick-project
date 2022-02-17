9ccb43aa1e948a412ffab3e5c1d19b317d25e2c3
Dan <danbenhur@protonmail.com>
2022-02-17 18:07:15 +0200

    added useRef

--- src/components/Employees/AddEmployee.js
+++ src/components/Employees/AddEmployee.js
@@ -2,38 +2,31 @@ import react from "react";
 import styles from "./AddEmployee.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ErrorModal from "../UI/ErrorModal";
 
 const AddEmployee = (props) => {
-  const [employeeNameInput, changeEmployeeInput] = useState("");
-  const [employeeIdInput, changeEmployeeId] = useState("");
+  const inputNameRef = useRef();
+  const inputIdRef = useRef();
+
   const [error, setError] = useState();
 
   const addEmployeeHandler = (event) => {
     event.preventDefault();
+    const enteredName = inputNameRef.current.value;
+    const enteredId = inputIdRef.current.value;
 
-    if (
-      employeeNameInput.trim().length === 0 ||
-      employeeIdInput.trim().length === 0
-    ) {
+    if (enteredName.trim().length === 0 || enteredId.trim().length === 0) {
       setError({
         title: "Invalid Input!",
-        message: "Please enter a valid Name and Id!",
+        message: "Please enter a valid Name and Id!!",
       });
       return;
     }
-    console.log(employeeNameInput, employeeIdInput);
-    props.onAddEmployee(employeeNameInput, employeeIdInput);
-    changeEmployeeInput("");
-    changeEmployeeId("");
-  };
 
-  const nameChangeHandler = (event) => {
-    changeEmployeeInput(event.target.value);
-  };
-  const idChangeHandler = (event) => {
-    changeEmployeeId(event.target.value);
+    props.onAddEmployee(enteredName, enteredId);
+    inputNameRef.current.value = "";
+    inputIdRef.current.value = "";
   };
 
   const errorHandler = () => {
@@ -52,19 +45,9 @@ const AddEmployee = (props) => {
       <Card className={styles.input}>
         <form onSubmit={addEmployeeHandler}>
           <label htmlFor="employeesname">Employee's Name</label>
-          <input
-            id="employeesname"
-            type="text"
-            value={employeeNameInput}
-            onChange={nameChangeHandler}
-          ></input>
+          <input id="employeesname" type="text" ref={inputNameRef}></input>
           <label htmlFor="employeesid">Enter Id</label>
-          <input
-            id="employeesid"
-            type="number"
-            onChange={idChangeHandler}
-            value={employeeIdInput}
-          ></input>
+          <input id="employeesid" type="number" ref={inputIdRef}></input>
           <Button type="submit">Add Employee</Button>
         </form>
       </Card>
